8d2779f56045c20e78a90b7064c76bc1a4142967
Agnes Bauer <agnesbauer@Agness-MacBook-Pro-2.local>
2026-08-30 20:26:01 +0200

    Integrate Reshaped UI library

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Reshaped } from 'reshaped'
+import 'reshaped/themes/slate/theme.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Reshaped theme="slate">
+      <App />
+    </Reshaped>
   </StrictMode>,
 )
